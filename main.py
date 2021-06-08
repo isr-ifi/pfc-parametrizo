5f8338bff15a2c2b7298a5cb15535f0958bb8d59
João Gonçalves <jsvgoncalves@gmail.com>
2021-06-08 16:13:54 +0200

    switch to bytes

--- main.py
+++ main.py
@@ -5,30 +5,31 @@ class Cluster:
     def __init__(self, cards: ACs, factors: list[float]):
         self.cards : ACs = cards
         self.factors : list = factors
-        self.played : str = "0000"
+        self.played : int = 0
+        self.factor = factors[0]
 
     def play(self, card: str):
-        i : int = 3 - self.cards.index(card) # index at other end (b0001)
-        self.played = self.played[:i] + "1" + self.played[i + 1:]
+        i : int = self.cards.index(card)
+        self.played = self.played ^ (1 << i)
+        self.factor = self.factors[self.played]
 
     def unplay(self, card: str):
-        i : int = 3 - self.cards.index(card) # index at other end (b0001)
-        self.played = self.played[:i] + "0" + self.played[i + 1:]
+        i : int = self.cards.index(card) # index at other end (b0001)
+        self.played = self.played ^ (1 << i)
+        self.factor = self.factors[self.played]
 
-    def factor(self, cards: ACs):
-        pass
 
 if __name__ == "__main__":
     c: Cluster = Cluster(["c1", "c2", "c3", "c4"],
             [x for x in range(16)])
+    assert c.factor == 0
     c.play("c1")
-    assert c.played == "0001"
+    assert c.played == 1
+    assert c.factor == 1
     c.play("c2")
-    assert c.played == "0011"
+    assert c.played == 3
+    assert c.factor == 3
     c.unplay("c1")
-    assert c.played == "0010"
-    #assert c.factor == 3
-    #assert c.factor(["c1"]) == 0.9
-    #assert c.factor(["c1", "c2"]) == 0.8
-    #assert c.factor(["c1", "c2"]) == 0.6
-    #assert c.factor(["c1", "c2"]) == 0.8
+    assert c.played == 2
+    assert c.factor == 2
+
